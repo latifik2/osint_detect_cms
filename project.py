c6d17c21946c094e8855a2593b4a0a475fa75171
latifik2 <latifik2@bk.ru>
2021-11-17 16:42:14 +1000

    v0.2

--- project.py
+++ project.py
@@ -9,6 +9,7 @@ def checkRobots(url):
     wp = "/wp-admin"
     joomla = "/administrator"
 
+    print("Analyzing robots.txt file...")
     request = requests.get(url)
     if(re.search(drupal, request.text) != None):
         print(f"There is {drupal} directory in robots.txt file\nSuggesting CMS - Drupal")
@@ -17,6 +18,14 @@ def checkRobots(url):
     if(re.search(joomla, request.text) != None):
         print(f"There is {joomla} directory in robots.txt file\nSuggesting CMS - Joomla")
 
+def checkMeta(url):
+    print("Analyzing meta information...")
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'lxml')
+    result = soup.find('head').find('meta', {'name':'generator'}).get('content')
+    
+    print(f"Found meta tag.\nSuggesting CMS - {result}")
+
 
 checkRobots(url)
-
+checkMeta(url)
